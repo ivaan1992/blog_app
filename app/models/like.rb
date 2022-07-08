023eaacdc617b27e191691acb3d33a14691f55c4
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-07-08 14:58:05 -0500

    Create models

---
+++ app/models/like.rb
@@ -0,0 +1,13 @@
+class Like < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+  
+    after_save :counting_like
+  
+    private
+  
+    def counting_like
+      post.update(likes_counter: post.likes.count)
+    end
+  
+end
